120d36c4607e5e7fb4d078d64d70d22816ad5b4a
vvh ff <ffvvh859@gmail.com>
2025-05-26 20:48:44 +0000

    موسوعة الحديث النبوي (بالإنجليزية والعربية)   تفسير القرآن الكريم (موقع آلوكة) احذف الجزئيتين دول وحسن الوجه اكثر من كده

--- src/data/linksData.ts
+++ src/data/linksData.ts
@@ -25,10 +25,10 @@ export const linkCategories: LinkCategory[] = [
         description: 'بودكاست ديني متنوع' 
       },
       { 
-        id: 'link_mp3_quran_bot', // Changed ID to reflect new link
-        text: 'القرآن الكريم بأصوات متعددة (بوت تيليجرام)', // Updated text
-        url: 'https://t.me/AAAMO2_bot?start=33989d3de6fcecfeed109956ffdf83d4', // Updated URL
-        description: 'بوت تيليجرام للاستماع للقرآن الكريم بأصوات مختلفة' // Updated description
+        id: 'link_mp3_quran_bot',
+        text: 'القرآن الكريم بأصوات متعددة (بوت تيليجرام)',
+        url: 'https://t.me/AAAMO2_bot?start=33989d3de6fcecfeed109956ffdf83d4',
+        description: 'بوت تيليجرام للاستماع للقرآن الكريم بأصوات مختلفة'
       },
       { 
         id: 'link_hijri_calendar_app', 
@@ -49,34 +49,22 @@ export const linkCategories: LinkCategory[] = [
     title: 'روابط إسلامية مفيدة',
     links: [
       { 
-        id: 'link_islamweb_tafsir', // Swapped with islamqa
+        id: 'link_islamweb_tafsir',
         text: 'تفسير القرآن الشامل (إسلام ويب)', 
         url: 'https://islamweb.net/ar/library/index.php?page=bookcontents&id=51&bk_no=51&flag=1', 
         description: 'تفسير مفصل للقرآن الكريم من موقع إسلام ويب' 
       },
       { 
-        id: 'link_islamqa', // Swapped with islamweb_tafsir
+        id: 'link_islamqa', 
         text: 'فتاوى إسلامية موثوقة (سؤال وجواب)', 
         url: 'https://islamqa.info/ar', 
         description: 'فتاوى وأسئلة وأجوبة إسلامية شاملة' 
-      },
-      { 
-        id: 'link_alukah_tafsir', 
-        text: 'تفسير القرآن الكريم (موقع آلوكة)', 
-        url: 'https://www.alukah.net/library/0/76937/', 
-        description: 'تفسير القرآن من موقع آلوكة' 
       },
       { 
         id: 'link_dorar_sunnah', 
         text: 'السنة النبوية الدرر السنية (أحاديث وأحكام)', 
         url: 'https://www.dorar.net/hadith', 
         description: 'موسوعة الحديث من الدرر السنية' 
-      },
-      { 
-        id: 'link_encyclopedia_hadith', 
-        text: 'موسوعة الحديث النبوي (بالإنجليزية والعربية)', 
-        url: 'https://sunnah.com/', 
-        description: 'موسوعة الحديث النبوي الشريف' 
       },
       { 
         id: 'link_ibn_baz', 
@@ -91,9 +79,9 @@ export const linkCategories: LinkCategory[] = [
         description: 'دروس ومحاضرات إسلامية' 
       },
       {
-        id: 'link_telegram_aaamo2_group', // Changed ID slightly to avoid conflict if AAAMO2_bot is used elsewhere
+        id: 'link_telegram_aaamo2_group',
         text: 'مجموعة تيليجرام إسلامية',
-        url: 'https://t.me/AAAMO2_bot', // User provided a bot link which might be intended for the group
+        url: 'https://t.me/AAAMO2_bot',
         description: 'مجموعة تيليجرام لمحتوى إسلامي متنوع'
       }
     ],
